67831fd0f9ed5334e406884ccde88206b5ac91fa
Maris Orbidans <maris.orbidans@onthemarket.com>
2020-07-11 14:55:44 +0300

    refactor

--- src/sub-array-sum.lisp
+++ src/sub-array-sum.lisp
@@ -1,16 +1,18 @@
 (defpackage :sub-array-sum
-  (:use cl alexandria serapeum)
-  (:export sub-array-sum sub-array-pos-sum sub-array-hash-sum))
+  (:use #:cl #:alexandria #:serapeum)
+  (:export #:sub-seq-sum #:sub-array-sum #:sub-array-pos-sum #:sub-array-hash-sum))
 
-(in-package :sub-array-sum)
+(in-package #:sub-array-sum)
 
-(defun array-sum (array from to)
-  (reduce #'+ array :start from :end to :initial-value 0))
+(defun sub-seq-sum (array from to)
+  (reduce #'+ array :start from :end (1+ to) :initial-value 0))
 
+;; O(n^2)
 (defun sub-array-sum (array target)
   (let* ((size (length array))
-	 (indices (loop :for x :below size :append (loop :for y :from (1+ x) :upto size :collect (cons x y)))))
-    (find-if (lambda (idxs) (= target (array-sum array (car idxs) (cdr idxs)))) indices)))
+	 (indices (loop :for x :below size
+			:append (loop :for y :from x :upto (1- size) :collect (cons x y)))))
+    (find-if (lambda (idxs) (= target (sub-seq-sum array (car idxs) (cdr idxs)))) indices)))
 
 (defvar *target*)
 (defvar *array*)
@@ -18,10 +20,11 @@
 
 (defun find-sub-array-pos-sum (start index sum)
   (when (<= index *array-size*)
-    (cond ((= sum *target*) (cons start index))
+    (cond ((= sum *target*) (cons start (1- index)))
 	  ((and (> sum *target*) (< start (1- index))) (find-sub-array-pos-sum (1+ start) index (- sum (aref *array* start))))
 	  ((< index *array-size*) (find-sub-array-pos-sum start (1+ index) (+ sum (aref *array* index)))))))
 
+;; O(n)
 ;; works for non-negative numbers only
 (defun sub-array-pos-sum (array target)
   (let ((*target* target)
@@ -29,12 +32,22 @@
 	(*array-size* (length array)))
     (find-sub-array-pos-sum 0 1 (aref array 0))))
 
-(defun find-sub-array-hash-sum (idx-sum-hash sum idx)
-  (let ((s (gethash (- sum *target*) idx-sum-hash)))
-    (cond (s (cons (1+ s) idx))
-	  ((< idx *array-size*) (let* ((next-sum (+ sum (aref *array* idx))))
-				  (setf (gethash next-sum idx-sum-hash) idx)
-				  (find-sub-array-hash-sum idx-sum-hash next-sum (1+ idx)))))))
+;; O(n)
+;; calculate current sum s(i) for each index i
+;; result is found if s(p) = target - current sum for any of previous indices p
+;; index 0  1  2  3  4  5
+;; elem  1  3  2  5  3  1
+;; sum   1  4  6  11 14 15
+;; sum (1 to 3) = s(3) - s(0) = 11 - 1 = 10
+
+(defun find-sub-array-hash-sum (sum-idx-hash sum idx)
+  (let* ((next-sum (+ sum (aref *array* idx)))
+	 (i (gethash (- next-sum *target*) sum-idx-hash)))
+    (cond (i
+	   (cons (1+ i) idx))
+	  ((< idx *array-size*)
+	   (setf (gethash next-sum sum-idx-hash) idx)
+	   (find-sub-array-hash-sum sum-idx-hash next-sum (1+ idx))))))
 
 (defun sub-array-hash-sum (array target)
   (let ((*target* target)
